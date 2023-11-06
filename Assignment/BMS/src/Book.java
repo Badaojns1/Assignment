1f1f2574eb92a1e9e57ec8ae453ab2cfd35aae4c
Badaojns1 <114817482+Badaojns1@users.noreply.github.com>
2023-11-06 21:46:16 +0700

    Add files via upload

---
+++ Assignment/BMS/src/Book.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author thang
+ */
+public class Book {
+    public String code;
+    public String title;
+    public int qua;
+    public double price;
+
+    public Book() {
+    }
+
+    public Book(String code, String title, int qua, double price) {
+        this.code = code;
+        this.title = title;
+        this.qua = qua;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getQua() {
+        return qua;
+    }
+
+    public void setQua(int qua) {
+        this.qua = qua;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s, %s, %d, %.2f)", code,title,qua,price);
+    }
+    
+}
